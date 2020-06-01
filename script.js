6794c1e62e590a61535f7ed91294eb0b8af605b6
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-06-01 13:03:05 -0500

    used classList

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 document.addEventListener('keydown', function(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    if(!audio) return;
-    audio.play()
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`) //This will bring about the animation for the key that is hit.
+    if(!audio) return; //stops function from running if their is no audio concurrent with the key
+    audio.currentTime = 0; //rewind to the start of the sound when you hit the key repeatedly
+    audio.play() 
+    key.classList.add('play') //this will add the class when the keys are hit
 })
