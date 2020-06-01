9d3621921037090eaaa8535b11b04e39466986d1
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-06-01 13:18:16 -0500

    completed

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-document.addEventListener('keydown', function(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`) //This will bring about the animation for the key that is hit.
-    if(!audio) return; //stops function from running if their is no audio concurrent with the key
-    audio.currentTime = 0; //rewind to the start of the sound when you hit the key repeatedly
-    audio.play() 
-    key.classList.add('play') //this will add the class when the keys are hit
-})
+function playSound(e){
+    
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`) //This will bring about the animation for the key that is hit.
+        if(!audio) return; //stops function from running if their is no audio concurrent with the key
+        audio.currentTime = 0; //rewind to the start of the sound when you hit the key repeatedly
+        audio.play() 
+        key.classList.add('playing') //this will add the class when the keys are hit
+    }
+
+
+document.addEventListener('keydown', playSound)
+function removeTransition(e){
+    if(e.propertyName !== "transform") return; //skip it if its not a transform
+    this.classList.remove('playing') //This will remove the CSS effects which are triggered when the keys are hit
+}
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', removeTransition)) //transitionend event is built in event like "click" or "keydown"
